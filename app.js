2a729dc700117dfb0348023b138a0d586527096e
Ajay Shankar <ajayshankar65@yahoo.com>
2023-12-11 20:23:54 -0500

    adds remaining endpoints

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const options = {
 const specs = swaggerJsdoc(options);
 
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(specs));
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hi, my name is Ajay Shankar. Welcome to my final project.')
@@ -46,7 +47,6 @@ app.get('/dataset', (req, res)=> {
     res.redirect('https://centraluseuap.dev.cognitive.microsoft.com/docs/services/unified-vision-apis-public-preview-2023-02-01-preview/operations/63c227a15ddd17166ce42740');
 });
 
-
 /**
  * @swagger
  * /datasets:
@@ -64,10 +64,117 @@ app.get('/datasets', (req, res) => {
     res.redirect('https://centraluseuap.dev.cognitive.microsoft.com/docs/services/unified-vision-apis-public-preview-2023-02-01-preview/operations/63c227a15ddd17166ce42743');
 });
 
+/**
+ * @swagger
+ * /model:
+ *      get: 
+ *          description: retrieves information about one model
+ *          produces:   
+ *              - application/json
+ *          responses:
+ *              200:
+ *                  description: Operation completed successfully
+ *              400: 
+ *                  description: The request was malformed
+ *              404:
+ *                  description: A model with the specified name was not found
+ */
+app.get('/model', (req, res) => {
+    res.redirect('https://centraluseuap.dev.cognitive.microsoft.com/docs/services/unified-vision-apis-public-preview-2023-02-01-preview/operations/63c227a15ddd17166ce42745');
+});
 
-app.use(cors);
+/**
+ * @swagger
+ * /models:
+ *      get: 
+ *          description: retrieves all models
+ *          produces:   
+ *              - application/json
+ *          responses:
+ *              200:
+ *                  description: Operation completed successfully
+ *              400: 
+ *                  description: The request was malformed
+ */
+app.get('/models', (req, res) => {
+    res.redirect('https://centraluseuap.dev.cognitive.microsoft.com/docs/services/unified-vision-apis-public-preview-2023-02-01-preview/operations/63c227a15ddd17166ce42747');
+});
+
+/**
+ * @swagger
+ * /evaluation:
+ *      get: 
+ *          description: retrieves information about one model evaluation
+ *          produces:   
+ *              - application/json
+ *          responses:
+ *              200:
+ *                  description: Operation completed successfully
+ *              400: 
+ *                  description: The request was malformed
+ *              404:
+ *                  description: A model evaluation with the specified name was not found
+ */
+app.get('/evaluation', (req, res) => {
+    res.redirect('https://centraluseuap.dev.cognitive.microsoft.com/docs/services/unified-vision-apis-public-preview-2023-02-01-preview/operations/63c227a15ddd17166ce4274a');
+});
+
+/**
+ * @swagger
+ * /evaluations:
+ *      get: 
+ *          description: retrieves all model evaluations
+ *          produces:   
+ *              - application/json
+ *          responses:
+ *              200:
+ *                  description: Operation completed successfully
+ *              400: 
+ *                  description: The request was malformed
+ */
+app.get('/evaluations', (req, res) => {
+    res.redirect('https://centraluseuap.dev.cognitive.microsoft.com/docs/services/unified-vision-apis-public-preview-2023-02-01-preview/operations/63c227a15ddd17166ce4274c');
+});
+
+/**
+ * @swagger
+ * /model:
+ *      delete: 
+ *          description: deletes a custom model
+ *          produces:   
+ *              - application/json
+ *          responses:
+ *              200:
+ *                  description: Operation completed successfully
+ *              400: 
+ *                  description: The request was malformed
+ *              404:
+ *                  description: A model with the specified name was not found
+ */
+app.delete('/model', (req, res) => {
+    res.redirect('https://centraluseuap.dev.cognitive.microsoft.com/docs/services/unified-vision-apis-public-preview-2023-02-01-preview/operations/63c227a15ddd17166ce42746');
+});
+
+/**
+ * @swagger
+ * /evaluation:
+ *      delete: 
+ *          description: deletes a model evaluation
+ *          produces:   
+ *              - application/json
+ *          responses:
+ *              200:
+ *                  description: Operation completed successfully
+ *              400: 
+ *                  description: The request was malformed
+ *              404:
+ *                  description: A model with the specified name was not found
+ */
+app.delete('/evaluation', (req, res) => {
+    res.redirect('https://centraluseuap.dev.cognitive.microsoft.com/docs/services/unified-vision-apis-public-preview-2023-02-01-preview/operations/63c227a15ddd17166ce4274b');
+});
 
 
 app.listen(port, () => {
-    console.log("API served at localhost" + port)
+    console.log("App is listening on port " + port)
 });
